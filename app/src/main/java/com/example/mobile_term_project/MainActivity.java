299f1f6dde1008f1deb99cb0c7321bb96590cc23
EdensHand <mack.j.tully@gmail.com>
2025-02-24 12:35:33 -0600

    Removed unused code

--- app/src/main/java/com/example/mobile_term_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_term_project/MainActivity.java
@@ -1,32 +1,28 @@
 package com.example.mobile_term_project;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements
-        View.OnTouchListener,
-        GestureDetector.OnGestureListener {
+        View.OnTouchListener
+    {
 
-    private GestureDetector mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        mGestureDetector = new GestureDetector(this,this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,9 +31,9 @@ public class MainActivity extends AppCompatActivity implements
         });
     }
     public float x,y;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
         switch(event.getAction()) {
             case (MotionEvent.ACTION_DOWN) :
                 //Log.d("DEBUG_TAG","Action was DOWN");
@@ -74,40 +70,5 @@ public class MainActivity extends AppCompatActivity implements
             default :
                 return super.onTouchEvent(event);
         }
-    }
-    @Override
-    public boolean onDown(@NonNull MotionEvent e) {
-        Log.d("IDK", "onDown: called");
-        return false;
-    }
-
-    @Override
-    public void onShowPress(@NonNull MotionEvent e) {
-        Log.d("IDK", "onShowPress: called");
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(@NonNull MotionEvent e) {
-        Log.d("IDK", "onSingleTapUp: called");
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
-        Log.d("IDK", "onScroll: called");
-        return false;
-    }
-
-    @Override
-    public void onLongPress(@NonNull MotionEvent e) {
-        Log.d("IDK", "onLongPress: called");
-
-    }
-
-    @Override
-    public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-        Log.d("IDK", "onFling: called");
-        return false;
     }
 }
