4904440b71ec5459dab3fdacdd7b09422021d11e
WhiteAlejandro <mr.white_alejandro@icloud.com>
2025-03-05 10:19:25 -0600

    python

---
+++ backend.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Path to the service account key file
+cred = credentials.Certificate("./touchalytics-27705-firebase-adminsdk-fbsvc-faa52519b1.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://touchalytics-27705-default-rtdb.firebaseio.com/'
+})
+
+# Writing data to the database
+user = input("Please enter the username of the individual whose data you'd like to retrieve: ")
+print()
+ref = db.reference(user)
+message = ref.get()
+if message == None:
+    print(f"User \"{user}\" does not exist in the database.")
+else:
+    print(f"Read from Firebase: {message}")
+print()
